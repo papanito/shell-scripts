530071ae99d8b59a9235086e223fb77b9f06e680
Papanito <papanito@wyssmann.com>
2017-04-05 13:59:55 +0200

    Removed unecessary char at the end of the script

--- backup_simple.sh
+++ backup_simple.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Simple backup script to save essential configurations and data as tar file
 
 today=`date '+%Y%m%d'`
 now=`date '+%Y%m%d%H%M'`
@@ -8,7 +9,7 @@ if [ ! -d "$backupdir" ]; then
 	mkdir $backupdir
 fi
 
+echo Backup will be stored at $backupdir
 tar czfv $backupdir/var-lib-mysql-$now.tar.gz /var/lib/mysql/
 tar czfv $backupdir/var-www-$now.tar.gz /var/www/
 tar czfv $backupdir/etc-$now.tar.gz /etc/
-~
