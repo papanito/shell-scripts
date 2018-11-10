86a952aa8c734d1a8f6f96f3bfdd2cc7af9c7a96
papanito <papanito@wyssmann.com>
2018-11-10 17:11:30 +0100

    Fix iteration trough folder

--- gnome-extensions-enable.sh
+++ gnome-extensions-enable.sh
@@ -1,3 +1,3 @@
-for i in ~/.local/share/gnome-shell/extensions;do gnome-shell-extension-tool -e "$i";done
+for i in ~/.local/share/gnome-shell/extensions/*;do gnome-shell-extension-tool -e "$i";done
 gnome-shell-extension-tool -e /usr/share/gnome-shell/extensions/GPaste@gnome-shell-extensions.gnome.org
 gnome-shell-extension-tool -e /usr/share/gnome-shell/extensions/auto-move-windows@gnome-shell-extensions.gcampax.github.com
