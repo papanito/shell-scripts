b339fc34abd6aa50f4a517060a7faf00697f891f
papanito <papanito@wyssmann.com>
2020-11-12 11:44:12 +0100

    Cleanup of scripts

--- scripts/scripts/gnome-extensions-enable.sh
+++
@@ -1,4 +0,0 @@
-localPath=~/.local/share/gnome-shell/extensions/
-for i in $localPath/*;do echo $(basename $i);gnome-extensions enable "$(basename $i)";done
-gnome-extensions enable GPaste@gnome-shell-extensions.gnome.org
-gnome-extensions enable gnome-shell-extensions.gcampax.github.com
